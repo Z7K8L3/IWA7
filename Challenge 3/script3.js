9d8c97e33a40bad85786e2a296cdfae9e32bd122
Zukile Lubanga <zukile.81.lubanga@gmail.com>
2023-08-16 23:45:07 +0200

    Updated Challenge 3 code.

--- Challenge 3/script3.js
+++ Challenge 3/script3.js
@@ -1,26 +1,28 @@
-const leoName = 'Leo';
-const leoSurname = 'Musvaire';
-const leoBalance = '9394';
+const leoName = 'Leo'
+const leoSurname = 'Musvaire'
+const leoBalance = '-9394'
 
-const sarahName = 'Sarah';
-const sarahSurname = 'Kleinhans';
-const sarahBalance = '4582.2';
+const sarahName = 'Sarah    '
+const sarahSurname = 'Kleinhans'
+const sarahBalance = '-4582.2'
 
-const divider = '----------------------------------';
+const divider = '----------------------------------'
 
 // Only change below this line
 
 // Total owed amount
-const owed = parseFloat(leoBalance) +  parseFloat(sarahBalance);
+const owed = Math.abs(parseInt(leoBalance)) +  Math.abs(parseFloat(sarahBalance));
 
 // Names and owed amounts
-const leo = `${leoName} ${leoSurname} (Owed: R ${parseFloat(leoBalance).toFixed(2)})`;
-const sarah = `${sarahName} ${sarahSurname} (Owed: R ${parseFloat(sarahBalance).toFixed(2)})`;
+const leo = `${leoName} ${leoSurname} (Owed: R ${Math.abs(parseInt(leoBalance)).toFixed(2)})`;
+const sarah = `${sarahName.trim()} ${sarahSurname} (Owed: R ${Math.abs(parseFloat(sarahBalance)).toFixed(2)})`;
 
 // Total owed section
-const total = `${divider}\n Total amount owed: R ${owed.toFixed(2)}\n${divider}`;
+const total = `${divider}\n Total amount owed: ${owed.toLocaleString('en-ZA', {style: 'currency', currency: 'ZAR'})}\n${divider}`;
 
 // Final output
 const result = `\n${leo}\n${sarah}\n${total}`;
 
 console.log(result);
+
+// Math.abs
